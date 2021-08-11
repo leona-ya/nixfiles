66d6c1f96f10314168e7d9f50df44d4bb450c9df
Leo Maroni <git@em0lar.de>
2021-08-11 14:35:40 +0200

    Update nix flakes

--- packages/default.nix
+++ packages/default.nix
@@ -5,6 +5,5 @@ self: super:
   prometheus-borg-exporter = self.callPackage ./prometheus-borg-exporter { };
   e1mo-ask = self.callPackage ./e1mo-ask { };
   opendatamap-net = self.callPackage ./opendatamap-net { };
-  grafana = self.callPackage ./grafana.nix { };
 }
 
