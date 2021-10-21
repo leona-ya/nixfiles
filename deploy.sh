ed2ce03fe3bb79a747a8a2e93cfccc3d8d592684
Leo Maroni <git@em0lar.de>
2021-10-21 22:17:44 +0200

    group deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,13 @@
 while [[ $# -gt 0 ]]; do
   key="$1"
   case $key in
-      -h|--hosts)
-      HOSTS="$2"
+      -h|--host)
+      HOST="$2"
+      shift
+      shift
+      ;;
+      -g|--groups)
+      GROUP="$2"
       shift
       shift
       ;;
@@ -14,5 +19,18 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+
+if [ -z $GROUP ];
+then
+    HOSTS=".#$HOST"
+    echo "hst"
+else
+    JQ_ARGS=""
+    HOSTS=.#$(cat hosts/groups.json | jq -r ".$GROUP.hosts | join(\" .#\")")
+    echo "gr"
+fi
+
+echo $HOSTS
+
 rsync -r /home/em0lar/dev/nixfiles 10.151.4.6:/tmp -e "ssh -p 61337"
-ssh -A -p 61337 10.151.4.6 "cd /tmp/nixfiles && deploy -s --targets .#${HOSTS} && cd /tmp && rm -rf /tmp/nixfiles"
+ssh -A -p 61337 10.151.4.6 "cd /tmp/nixfiles && deploy -s --targets $HOSTS && cd /tmp && rm -rf /tmp/nixfiles"
