00d88c29c8e3f54a7d255ecc45e021fda6772b82
Leona Maroni <dev@leona.is>
2023-12-01 17:32:17 +0100

    profiles/desktop: sway/wofi: style

--- profiles/desktop/sway/rofi.nix
+++ profiles/desktop/sway/rofi.nix
@@ -4,7 +4,46 @@
   home-manager.users.leona = {
     programs.wofi = {
       enable = true;
-#      theme = "solarized";
+      style = ''
+        window {
+          margin: 0px;
+          background-color: #2D2A2E;
+        }
+
+        #input {
+          margin: 2.5px;
+          border: none;
+          color: #a0e300;
+          background-color: #3F3A40;
+        }
+
+        #inner-box {
+          margin: 2.5px;
+          border: none;
+          background-color: #2D2A2E;
+        }
+
+        #outer-box {
+          margin: 2.5px;
+          border: none;
+          background-color: #2D2A2E;
+        }
+
+        #scroll {
+          margin: 0px;
+          border: none;
+        }
+
+        #text {
+          margin: 2.5px;
+          border: none;
+          color: #FCFCFA;
+        } 
+
+        #entry:selected {
+          background-color: #3F3A40;
+        }
+      '';
     };
   };
 }
