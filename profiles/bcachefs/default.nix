6300a7b4a5e9ffec5e5e0aff00a8cf83f1074914
Leona Maroni <dev@leona.is>
2023-12-24 14:10:02 +0100

    profiles/bcachefs: update kernel to 6.7-rc7

--- profiles/bcachefs/default.nix
+++ profiles/bcachefs/default.nix
@@ -3,11 +3,11 @@
   boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor (pkgs.linux_testing.override {
     argsOverride = rec {
       src = pkgs.fetchurl {
-        url = "https://git.kernel.org/torvalds/t/linux-6.7-rc5.tar.gz";
-        sha256 = "sha256-8yrRuObgvY7tEdIj6EDRXaBGS/NBMUVHjaHxHgunYBE=";
+        url = "https://git.kernel.org/torvalds/t/linux-6.7-rc7.tar.gz";
+        sha256 = "sha256-0MkigNsD1vd21vZ/rwNeupBLxfKgSoB9+rd9HrzjmwI=";
       };
-      version = "6.7-rc5";
-      modDirVersion = "6.7.0-rc5";
+      version = "6.7-rc7";
+      modDirVersion = "6.7.0-rc7";
     };
   }));
 
