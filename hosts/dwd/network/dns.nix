fed6116a1b3ebbf8a5046ea529eb4b7fc8683802
Leona Maroni <dev@leona.is>
2023-12-29 14:59:18 +0100

    style: cleanup

--- hosts/dwd/network/dns.nix
+++ hosts/dwd/network/dns.nix
@@ -17,33 +17,4 @@
       metric_version = 2;
     };
   };
-
-  services.knot = {
-    enable = true;
-    extraConfig = ''
-      server:
-        listen: 127.0.0.11@53
-      acl:
-        - id: internal_transfer
-          address: 127.0.0.0/8
-          action: transfer
-        - id: local_update
-          address: 127.0.0.0/8
-          action: update
-      mod-rrl:
-        - id: default
-          rate-limit: 200   # Allow 200 resp/s for each flow
-          slip: 2           # Every other response slips
-      template:
-        - id: default
-          semantic-checks: on
-          global-module: mod-rrl/default
-        - id: primary_update
-          acl: [local_update, internal_transfer]
-      zone:
-        - domain: lan
-          file: "/var/lib/knot/zones/lan.zone"
-          template: primary_update
-    '';
-  };
 }
--- services/matrix-old/default.nix
+++
@@ -1,7 +0,0 @@
-{ ... }:
-
-{
-  imports = [
-    ./synapse.nix
-  ];
-}
