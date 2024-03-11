b1c6f399d05e04a1206b5bd0b377487e86110496
Leona Maroni <dev@leona.is>
2024-03-11 23:20:25 +0100

    flake: applyPatches on nixpkgs v1

--- hosts/default.nix
+++ hosts/default.nix
@@ -20,7 +20,7 @@
 
       in
       {
-        meta = {
+        meta = rec {
           nixpkgs = import inputs.nixpkgs {
             system = "x86_64-linux";
           };
@@ -31,7 +31,18 @@
           }) // lib.genAttrs [ "freyda" "thia" "turingmachine" ]
             (_: import inputs.nixpkgs-unstable {
               system = "x86_64-linux";
-            });
+            }) // {
+              thia = (import ((import inputs.nixpkgs-unstable { system = "x86_64-linux"; }).applyPatches {
+                name = "nixpkgs-patched-293864";
+                src = inputs.nixpkgs-unstable;
+                patches = [
+                  (nixpkgs.fetchpatch {
+                    url = "https://github.com/NixOS/nixpkgs/pull/293864/commits/91ae595ff875ae0106e5d375acbc809daaf03115.patch";
+                    hash = "sha256-2iojVOPgtYzX7czwSGb5gR3GTsuXbDs1paNz8D3WU+0=";
+                  })
+                ];
+              }) { system = "x86_64-linux"; });
+            };
 
           specialArgs = {
             inherit inputs;
