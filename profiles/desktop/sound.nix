dd4326ba056ce2f85d1d8a32228e000da4cd6e30
Leona Maroni <dev@leona.is>
2024-11-01 22:44:45 +0100

    feat(profiles/desktop): enable airplay

--- profiles/desktop/sound.nix
+++ profiles/desktop/sound.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  users.users.leona.packages = with pkgs; [ pavucontrol qjackctl ];
+  users.users.leona.packages = with pkgs; [ pwvucontrol qjackctl ];
 
   security.rtkit.enable = true;
   services.pipewire = {
@@ -10,6 +10,25 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-    #    wireplumber.enable = false;
+    extraConfig = {
+      pipewire-pulse = {
+        network-discover = {
+          "context.exec" = [
+            { path = "pactl"; args = "load-module module-native-protocol-tcp"; }
+            { path = "pactl"; args = "load-module module-zeroconf-discover"; }
+          ];
+        };
+      };
+      pipewire = {
+        raop-discover = {
+          "context.modules" = [
+            {
+              name = "libpipewire-module-raop-discover";
+              args = { };
+            }
+          ];
+        };
+      };
+    };
   };
 }
