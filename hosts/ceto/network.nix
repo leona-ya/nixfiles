7c818163877086056a29c43618083db463a371ce
Leona Maroni <dev@leona.is>
2025-04-11 20:41:24 +0000

    feat(h/ceto): add sfp network

--- hosts/ceto/network.nix
+++ hosts/ceto/network.nix
@@ -6,18 +6,36 @@
   networking.domain = "net.leona.is";
 
   systemd.network = {
-    links."10-eth0" = {
-      matchConfig.MACAddress = "9c:6b:00:97:b5:8a";
-      linkConfig = {
-        Name = "eth0";        
-        WakeOnLan = "magic";
+    links = { 
+      "10-eth0" = {
+        matchConfig.MACAddress = "9c:6b:00:97:b5:8a";
+        linkConfig = {
+          Name = "eth0";        
+          WakeOnLan = "magic";
+        };
+      };
+      "10-sfp0" = {
+        matchConfig.MACAddress = "ec:0d:9a:2c:22:30";
+        linkConfig = {
+          Name = "sfp0";
+          WakeOnLan = "magic";
+        };
       };
     };
-    networks."10-eth0" = {
-      DHCP = "yes";
-      matchConfig.Name = "eth0";
-      linkConfig = {
-        RequiredForOnline = "yes";
+    networks = {
+      "10-eth0" = {
+        DHCP = "yes";
+        matchConfig.Name = "eth0";
+        linkConfig = {
+          RequiredForOnline = "no";
+        };
+      };
+      "10-sfp0" = {
+        DHCP = "yes";
+        matchConfig.Name = "sfp0";
+        linkConfig = {
+          RequiredForOnline = "yes";
+        };
       };
     };
   };
