14eddc4e117a1dee6038b427b4ac52115ea234cd
Leona Maroni <dev@leona.is>
2025-07-17 09:08:56 +0200

    update: flake inputs

--- hosts/default.nix
+++ hosts/default.nix
@@ -26,10 +26,10 @@
             system = "x86_64-linux";
           };
 
-          nodeNixpkgs = lib.genAttrs [ "bij" "laurel" "sphere" ]
+          nodeNixpkgs = lib.genAttrs [ "bij" "laurel" "sphere" "thia" ]
             (_: import inputs.nixpkgs {
               system = "aarch64-linux";
-            }) // lib.genAttrs [ "ceto" "freyda" "thia" "turingmachine" ]
+            }) // lib.genAttrs [ "ceto" "freyda" "turingmachine" ]
             (_: import inputs.nixpkgs-unstable {
               system = "x86_64-linux";
             }) // rec {
@@ -56,18 +56,18 @@
                   name = "nixpkgs-patched-naya";
                   src = inputs.nixpkgs;
                   patches = [
-                    (nixpkgs.fetchpatch {
-                      url = "https://github.com/leona-ya/nixpkgs/commit/69f5b8d40fe18f4b709dc23cddacf60e07217222.patch";
-                      hash = "sha256-rkRxHCzd/wDvZ6NOsKeIAinz1AUXlq2yztOL/sK0ek4=";
-                    })
                   ];
                 })
                 { system = "x86_64-linux"; });
-              laurel = (import
+              sphere = (import
                 ((import inputs.nixpkgs { system = "x86_64-linux"; }).applyPatches {
                   name = "nixpkgs-patched-laurel";
                   src = inputs.nixpkgs;
                   patches = [
+                    (nixpkgs.fetchpatch {
+                      url = "https://github.com/NixOS/nixpkgs/pull/427122.patch";
+                      hash = "sha256-Hi7N6NKQ2tFITC8xyeKL9eSfVDl7W8F9RZJt1362QW0=";
+                    })
                   ];
                 })
                 { system = "aarch64-linux"; });
