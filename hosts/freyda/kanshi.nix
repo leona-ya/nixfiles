9bfe0689720db05be69b580211072638428813fd
Leona Maroni <dev@leona.is>
2025-09-20 21:59:08 +0200

    update: flake inputs

--- hosts/freyda/kanshi.nix
+++ hosts/freyda/kanshi.nix
@@ -8,18 +8,26 @@
         home = {
           outputs = [
             {
-              criteria = "eDP-1";
-              mode = "2256x1504";
-              position = "1128,1200";
-              scale = 1.2;
+              criteria = "Dell Inc. DELL U3223QE FDPTDP3";
+              mode = "3840x2160";
+              position = "1309,363";
+              scale = 1.35;
             }
             {
-              criteria = "DP-2";
-              mode = "3840x1200";
+              criteria = "Dell Inc. DELL P2423D JY45VZ3";
+              mode = "2560x1440";
               position = "0,0";
+              scale = 1.1;
+              transform = "270";
+            }
+            {
+              criteria = "eDP-1";
+              mode = "2256x1504";
+              position = "4153,500";
+              scale = 1.2;
             }
           ];
-          exec = "${pkgs.sway}/bin/swaymsg workspace 2, move workspace to output DP-2";
+          exec = "${pkgs.sway}/bin/swaymsg workspace 2, move workspace to output 'Dell Inc. DELL P2423D JY45VZ3'";
         };
         default = {
           outputs = [
