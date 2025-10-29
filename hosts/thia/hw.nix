0d6eb5b0512e05b07ea6b037e4b24c8ba4e55b73
Leona Maroni <dev@leona.is>
2025-10-29 12:24:24 +0100

    fix(h/thia): fan addresses for hwmon

--- hosts/thia/hw.nix
+++ hosts/thia/hw.nix
@@ -4,15 +4,15 @@
     enable = true;
     config = ''
       INTERVAL=10
-      DEVPATH=hwmon5=devices/platform/nct6775.656
-      DEVNAME=hwmon5=nct6793
-      FCTEMPS=hwmon5/pwm1=hwmon5/temp7_input
-      FCFANS= hwmon5/pwm1=hwmon5/fan1_input
-      MINTEMP=hwmon5/pwm1=30
-      MAXTEMP=hwmon5/pwm1=70
-      MINSTART=30 hwmon5/pwm1=56
-      MINSTOP=30 hwmon5/pwm1=16
-      MAXPWM=hwmon5/pwm1=150
+      DEVPATH=hwmon3=devices/platform/nct6775.656
+      DEVNAME=hwmon3=nct6793
+      FCTEMPS=hwmon3/pwm1=hwmon3/temp7_input
+      FCFANS= hwmon3/pwm1=hwmon3/fan1_input
+      MINTEMP=hwmon3/pwm1=30
+      MAXTEMP=hwmon3/pwm1=70
+      MINSTART=30 hwmon3/pwm1=56
+      MINSTOP=30 hwmon3/pwm1=16
+      MAXPWM=hwmon3/pwm1=150
     '';
   };
   boot.kernelModules = [ "nct6775" ];
