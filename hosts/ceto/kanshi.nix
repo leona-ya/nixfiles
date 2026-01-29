6d8c6b018c68ee0aad2c94ae0cb9138b57eb5250
Leona Maroni <dev@leona.is>
2026-01-29 12:25:45 +0100

    fix(h/ceto): add kanshi config without kvm

--- hosts/ceto/kanshi.nix
+++ hosts/ceto/kanshi.nix
@@ -27,6 +27,24 @@
           ];
           exec = "${pkgs.sway}/bin/swaymsg workspace 2, move workspace to output 'Dell Inc. DELL P2423D JY45VZ3'";
         };
+        home2 = {
+          outputs = [
+            {
+              criteria = "Dell Inc. DELL U3223QE FDPTDP3";
+              mode = "3840x2160";
+              position = "1307,363";
+              scale = 1.35;
+            }
+            {
+              criteria = "Dell Inc. DELL P2423D JY45VZ3";
+              mode = "2560x1440";
+              position = "0,0";
+              scale = 1.1;
+              transform = "270";
+            }
+          ];
+          exec = "${pkgs.sway}/bin/swaymsg workspace 2, move workspace to output 'Dell Inc. DELL P2423D JY45VZ3'";
+        };
       };
     };
     wayland.windowManager.sway.config.startup = [
