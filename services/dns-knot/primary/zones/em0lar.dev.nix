d3267324d80114d8c7abd13dc79a4c6e03eea247
Leona Maroni <dev@leona.is>
2026-05-05 15:49:55 +0200

    chore: cleanup dns

--- services/dns-knot/primary/zones/em0lar.dev.nix
+++ services/dns-knot/primary/zones/em0lar.dev.nix
@@ -8,12 +8,6 @@
 }:
 
 with dns.lib.combinators;
-
-let
-  haku_host = host "195.39.247.188" "2a0f:4ac0:0:1::d25";
-  naiad_host = host "37.120.184.164" "2a03:4000:f:85f::1";
-  kupe_host = host "159.69.17.61" "2a01:4f8:1c1c:f0b::1";
-in
 {
   zone = {
     TTL = 3600;
@@ -51,10 +45,6 @@ in
     subdomains = hosthelper.services.dns-int.g_dns_records // {
       "wg.net".CNAME = [ "haku.net.leona.is." ];
 
-      "ns1" = kupe_host;
-      "ns2" = haku_host;
-      "ns3" = naiad_host;
-
       mail.CNAME = [ (ttl (60 * 5) (cname "koyo.net.infinitespace.dev.")) ];
       autoconfig.CNAME = [ "koyo.net.infinitespace.dev." ];
       "wg-sternpunkt".CNAME = [ "wg.net.leona.is." ]; # backwards compatability
@@ -67,10 +57,6 @@ in
           p = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2WJ46bl9UqBY9ZxqkVCBdSiysIJMUbWS3BK10Lupe4T5+jWAcdzJraznWeaVF/mR/9TyiB7lE79ZB6WxHxTwwJ5UZjURwImKAKqSGPXPACIj+LHyx5j2nHN4CawC6bkCmpGT99B7I/5bCelekoAHV9U/4pE2YEjgA0VxvlSKHB2Y7cPWL303DInYGaTrvMczuwLYoEwIiBirffYNqHyrOJE9A+ZQRdLjM8DFOxegAOV9mcHb3MwneJuu86Czz45UIrQ7AxkMUNKgHitqTSnXzLWd4BF6Kf3XUh/lED7WPdviBLJo/1H0Cgch8RRlinTeDVliHDQ6/zLWpk6+k3iKkQIDAQAB";
         }
       ];
-
-      www.CNAME = [ "foros.net.leona.is." ];
-      auth.CNAME = [ "sso.leona.is." ];
-      git.CNAME = [ "beryl.net.leona.is." ];
     };
   };
 }
