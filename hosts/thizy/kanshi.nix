2f355d8299142e458b9bfc8a5ccca4c693927e39
Leona Maroni <dev@leona.is>
2026-08-17 13:51:24 +0200

    fix(h/thizy): kanshi profile for home

--- hosts/thizy/kanshi.nix
+++ hosts/thizy/kanshi.nix
@@ -15,6 +15,22 @@
             }
           ];
         };
+        home = {
+          outputs = [
+            {
+              criteria = "eDP-1";
+              mode = "2880x1800";
+              position = "0,0";
+              scale = 1.45;
+            }
+            {
+              criteria = "DP-1";
+              mode = "3840x2160";
+              position = "1986,0";
+              scale = 1.3;
+            }
+          ];
+        };
       };
     };
     wayland.windowManager.sway.config.startup = [
